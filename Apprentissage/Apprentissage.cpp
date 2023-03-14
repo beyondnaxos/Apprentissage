bdd484154ac854418181373d6e6dfb6dd4378ae8
BeyondNaxos <adopteuntrip@gmail.com>
2023-03-14 17:26:51 +0100

    Adding new instances

--- Apprentissage/Apprentissage.cpp
+++ Apprentissage/Apprentissage.cpp
@@ -25,14 +25,31 @@ public:
 
 int main() {
 
+	
 	Book harry = Book("Jk Rowling", "Fary Potter", 2023, true);
-	// create 10 other books 
-	Book indiana = Book
+	Book indiana = Book("Jean Pierr", "Indiana Jones", 1986, false);
+	Book mock = Book("Harper Lee", "To Kill a Mockingbird", 1960, true);
+	Book gatsby = Book("F. Scott Fitzgerald", "The Great Gatsby", 1925, true);
+	Book rye = Book("J.D. Salinger", "The Catcher in the Rye", 1951, false);
+	Book orwell = Book("George Orwell", "1984", 1949, true);
+	Book men = Book("John Steinbeck", "Of Mice and Men", 1937, true);
+	
+	
+
+		
 	
 	harry.myBookIs();
+	indiana.myBookIs();
+	mock.myBookIs();
+	gatsby.myBookIs();
+	rye.myBookIs();
+	orwell.myBookIs();
+	men.myBookIs();
+	
 
+	return 0;
+}
 
-};
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
 // Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
